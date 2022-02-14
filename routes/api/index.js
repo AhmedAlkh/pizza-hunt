f875413c16c02a023dc12b88f86be21283f98d05
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-02-13 19:31:37 -0500

    updated typo in index js, succesfully tested routes in insomnia

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use('./pizzas', pizzaRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
